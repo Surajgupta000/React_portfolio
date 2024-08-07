2bfa238f2de1f341e222f022d3036a72eae27ead
suraj kumar <sooraj4475@gmail.com>
2024-08-07 22:31:28 +0530

    new

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Tsection from "../../components/Tsection";
 
 export default function About() {
@@ -12,7 +11,6 @@ export default function About() {
     "Java",
     ".Net",
   ];
-
   return (
     <div>
       <Tsection title="About" />
@@ -27,10 +25,10 @@ export default function About() {
           ></dotlottie-player>
         </div>
         <div className="flex flex-col gap-10 w-1/2 sm:w-full">
-          <p className="text-white">
-            • Welcome to My Portfolio! <br /> Hi, I'm Suraj, a passionate and
+          <p className="text-white ">
+            • Welcome to My Portfolio! <br></br>Hi, I'm Suraj, a passionate and
             dedicated web developer with a flair for creating dynamic and
-            responsive web applications. With expertise in the MERN stack, I
+            responsive web applications. With expertise in the Mern stack, I
             specialize in building robust, user-friendly, and visually appealing
             websites. I have hands-on experience with modern technologies like
             React, Tailwind CSS, and Node.js, ensuring that my projects are
@@ -38,8 +36,8 @@ export default function About() {
             sleek single-page application or a complex web platform, I have the
             skills and creativity to bring your vision to life.
           </p>
-          <p className="text-white">
-            • Besides my technical prowess, I take pride in delivering clean,
+          <p className="text-white ">
+            •Besides my technical prowess, I take pride in delivering clean,
             maintainable code and a seamless user experience. I'm constantly
             learning and adapting to new trends and technologies to stay ahead
             in the ever-evolving field of web development. Explore my portfolio
@@ -52,14 +50,14 @@ export default function About() {
 
       <div className="py-5">
         <h1 className="text-tertiary text-xl">
-          Here are a few technologies I have worked on.
+          Here are few Technologies i have working on.
         </h1>
 
-        <div className="flex flex-wrap gap-10 mt-5">
+        <div className="flex flex-wrap gap-10 mt-5 ">
           {skills.map((skill, index) => (
-            <Link key={index} to={`/skills/${skill.toLowerCase()}`} className="border border-tertiary py-3 px-5">
+            <div key={index} className="border border-tertiary py-3 px-5">
               <h1 className="text-tertiary">{skill}</h1>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
