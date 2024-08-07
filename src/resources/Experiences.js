be4c34466871d0cc650a882677c1949b3057b945
suraj kumar <sooraj4475@gmail.com>
2024-08-07 22:31:59 +0530

    new

--- src/resources/Experiences.js
+++ src/resources/Experiences.js
@@ -2,17 +2,17 @@ export const Experiences = [
   {
     _id: "001",
     company: "AspireVision Tech Edu",
-    title: "Web Developer- Trainee",
-    period: "2021-2022",
+    title: "Web Developer_Trainee",
+    period: "2021-22",
     description:
       "Collaborated with senior developers to implement key features and functionalities in web projects, contributing to a 15percentage improvement in project completion efficiency Assisted in troubleshooting and debugging code issues, resolving an average of 10 bugs per week thereby enhancing project stability",
   },
   {
     _id: "002",
     company: "AspireVision Tech Edu",
-    title: "Front-end developer- intern",
-    period: "2022-2023",
-    description: "I will write here later>>>",
+    title: "Full stack developer_intern",
+    period: "2022-23",
+    description: "• Assisted in troubleshooting and debugging code issues, resolving an average of 10 bugs per week,thereby enhancing project stability Participated in team meetings and brainstorming sessions, contributing ideas for project enhancements and optimizations, resulting in a 20 percentage increase in overall project effectiveness",
   },
   {
     _id: "003",
