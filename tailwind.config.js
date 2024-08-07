15aff9c391732fa52a486e4258e7ec0249b4ec57
suraj kumar <sooraj4475@gmail.com>
2024-08-07 22:31:15 +0530

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        "primary" : "#0A192f",
+      colors: {
+        "primary": "#0A192f",
         "seconadry": "#f97316",
         "tertiary": "#009aee"
-      }
+      },
+      fontSize: {
+        '1.5xl': '1.375rem', // Add the custom font size between 1xl (1.25rem) and 2xl (1.5rem)
+      },
     },
     screens: {
-      
-
       'lg': {'max': '2023px'},
       // => @media (max-width: 1023px) { ... }
-
-
 
       'sm': {'max': '1000px'},
       // => @media (max-width: 639px) { ... }
